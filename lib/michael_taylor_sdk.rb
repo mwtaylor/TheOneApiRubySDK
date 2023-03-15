8ad2c665f53e130401026588750d31a1bee05cd5
Michael Taylor <mwtaylor@users.noreply.github.com>
2023-03-14 17:36:52 -0700

    Setup initial structure using bundle gem michael_taylor_sdk

---
+++ lib/michael_taylor_sdk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "michael_taylor_sdk/version"
+
+module MichaelTaylorSdk
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/michael_taylor_sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MichaelTaylorSdk do
+  it "has a version number" do
+    expect(MichaelTaylorSdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
